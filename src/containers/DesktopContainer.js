8c3dcfb03b40855e55100e7f9f5ed472ab7489f7
Chif Gergő <chifgeri97@gmail.com>
2019-02-18 23:18:19 +0100

    Add eslintrc. It extends the ruleset of fbjs. Fix linter errors

--- src/containers/DesktopContainer.js
+++ src/containers/DesktopContainer.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { Container, Responsive } from 'semantic-ui-react';
+import { Responsive } from 'semantic-ui-react';
 import DesktopMenu from '../components/menus/DesktopMenu';
 
 class DesktopContainer extends Component {
 
-  render(){
+  render() {
     const { children } = this.props;
     return (
-    <Responsive minWidth={768}>
-      <DesktopMenu />
-      {children}
-    </Responsive>
-    )
+      <Responsive minWidth={768}>
+        <DesktopMenu />
+        {children}
+      </Responsive>
+    );
   }
 }
 
