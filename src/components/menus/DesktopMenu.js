aa4df74e4d7bf528749a9b7dd123b317017cd4ea
Chif Gergő <chifgeri97@gmail.com>
2019-06-25 21:25:19 +0200

    Fix merge conflict

--- src/components/menus/DesktopMenu.js
+++ src/components/menus/DesktopMenu.js
@@ -11,16 +11,11 @@ const DesktopMenu = ({user}) => (
       < Input className='icon' icon='search' placeholder='Search...' />
     </Menu.Item>
     <Menu.Item position='right'>
-<<<<<<< Updated upstream
       {user.id ? user.full_name : ''}
     </Menu.Item>
     <Menu.Item>
       { user.id ?
         <Button as='a' href='/api/v1/logout'>Log out</Button>
-=======
-      { user.username ?
-        <Button as="a" href='/api/v1/logout/'>Log out</Button>
->>>>>>> Stashed changes
         :
         <Button as='a' href='/api/v1/login/authsch'>Log in</Button>
       }
