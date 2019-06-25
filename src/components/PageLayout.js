356e50afd1757df32b158522cf908aac782d6fe1
Chif Gergő <chifgeri97@gmail.com>
2019-06-25 21:48:56 +0200

    Fix footer

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -3,8 +3,10 @@ import DesktopContainer from '../containers/DesktopContainer';
 import Footer from './Footer';
 
 const PageLayout = ({children}) => (
-  <div>
-    <DesktopContainer >{children}</DesktopContainer>
+  <div style={{ minHeight: '100%', position: 'relative' }}>
+    <div id='main' style={{ minHeight: '100%', position: 'relative' }}>
+      <DesktopContainer >{children}</DesktopContainer>
+    </div>
     <Footer />
   </div>
 );
