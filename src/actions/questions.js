c7e7e9b663bba31940236e50f1a3d416350442fd
Chif Gergő <chifgeri97@gmail.com>
2019-07-02 22:17:35 +0200

    Create add action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,10 +1,21 @@
 import axios from './session';
+import { ADD_QUESTION } from './types';
 
-const addQuestion = ({title, details}) => (
+export const addQuestion = ({title, text}) => (
 
   async (dispatch) => {
+    try {
+      const response = await axios.post('api/v1/questions', { title, text });
 
+      if (response.status == '200') {
+        dispatch({
+          type: ADD_QUESTION,
+          payload: response.data,
+        });
+      }
+    } catch(e) {
+      console.log(e);
+    }
   }
+);
 
-)
-}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,2 @@
 export const GET_USER = 'get_user';
+export const ADD_QUESTION = 'add_question';
