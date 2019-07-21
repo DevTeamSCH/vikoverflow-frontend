6ff5ad0198faa9e70ded5673d2215b628038df94
Daniel Gal <daniel.gal@sch.bme.hu>
2019-07-21 16:06:12 +0200

    Touch up

--- src/components/Forbidden.js
+++ src/components/Forbidden.js
@@ -5,11 +5,12 @@ import { Link } from 'react-router-dom';
 const Forbidden = () => {
   return (
     <Grid>
-      <Grid.Column width={5}></Grid.Column>
-      <Grid.Column width={6}>
-        <Segment raised textAlign='center'>
-          <Label attached='top' size='large' color='red'>Sorry, you're not allowed here</Label>
-          <Header size='large'>Contact the devs to gain access</Header>
+      <Grid.Column width={4}></Grid.Column>
+      <Grid.Column width={8}>
+        <Segment raised padded textAlign='center'>
+          <Label attached='top' size='large' color='red'>Oops</Label>
+          <Header size='large'>Sorry, you're not allowed here</Header>
+          <p>Contact the devs to gain access</p>
           <Link to='/welcome'>Back to welcome page</Link>
         </Segment>
       </Grid.Column>
