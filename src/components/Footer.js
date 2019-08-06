33ee9895c82db8eaa06294b2bee5e18ed066182c
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2019-08-06 23:00:10 +0200

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Container, Segment } from 'semantic-ui-react';
 
 
 const Footer = () => (
-  <footer id='footer' style={{ position: 'absolute', width: '100%', bottom: '0', maxHeight: '2em' }}>
+  <footer id='footer' style={{ position: 'absolute', width: '100%', bottom: '0' }}>
     <Segment inverted vertical textAlign='center' >
       <Container>
         <p textalign='center' >Created by DevTeam &copy; 2018-2019.</p>
