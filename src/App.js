95b7e6c2b8b354f53aa5bf82adfdabee37bd1d42
Daniel Gal <daniel.gal@sch.bme.hu>
2019-11-04 00:03:22 +0100

    Router working as planned

--- src/App.js
+++ src/App.js
@@ -4,8 +4,49 @@ import { connect } from 'react-redux';
 
 import { getUserData } from './actions';
 
-const HomePage = () => (<div><h1>Home</h1><a href='/api/v1/logout'>Logout</a></div>);
 const LoginPage = () => (<div><h1>Login</h1><a href='/api/v1/login/authsch'>Login</a></div>);
+
+function Nav() {
+  return (
+    <div style={{ display: 'flex', direction: 'flex-row', marginLeft: '-5px' }}>
+      <div style={{ margin: '0 5px' }}><Link to='/'>Home</Link></div>
+      <div style={{ margin: '0 5px' }}><Link to='/questions'>Questions</Link></div>
+      <div style={{ margin: '0 5px' }}><Link to='/about'>About</Link></div>
+    </div>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Nav />
+      {children}
+      <a href='/api/v1/logout'>Logout</a>
+    </div>
+  );
+}
+
+function HomePage() {
+  return <div><h1>Home</h1></div>;
+}
+
+function AboutPage() {
+  return <div><h1>About</h1></div>;
+}
+
+function QuestionsPage() {
+  return <div><h1>Questions</h1></div>;
+}
+
+function AuthorizedApp() {
+  return (
+    <Layout>
+      <Route exact path='/questions' component={QuestionsPage} />
+      <Route exact path='/about' component={AboutPage} />
+      <Route exact path='/' component={HomePage} />
+    </Layout>
+  );
+}
 
 function ForbiddenPage() {
   return <div><h1>Forbidden</h1><Link to='/login'>Back to login page</Link></div>
@@ -16,9 +57,9 @@ function App({ user, getUserData }) {
   return (
     <Router>
       <Switch>
-        <Route exact path='/'>{user.id ? <HomePage /> : <Redirect to='/login' />}</Route>
         <Route exact path='/login'>{user.id ? <Redirect to='/' /> : <LoginPage />}</Route>
         <Route exact path='/forbidden'><ForbiddenPage /></Route>
+        <Route path='/'>{user.id ? <AuthorizedApp /> : <Redirect to='/login' />}</Route>
       </Switch>
     </Router>
   );
