fad508ad8e09c4af65b79647138cb1633906b2e2
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2019-11-12 01:03:10 +0100

    Add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,8 +5,8 @@ import style from './Layout.module.css';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Footer from "./Footer";
 import Nav from "react-bootstrap/Nav";
+import {APP_VERSION} from "../version";
 
 function TempNav() {
   return (
@@ -20,7 +20,7 @@ function TempNav() {
 export default function Layout({ children }) {
   return (
     <div>
-      <Row>
+      <Row className={style.content}>
         <Col md={2} className={"d-none d-md-block bg-light " + style.sidebar} as="nav">
           <div className={style.sidebarSticky}>
             <ul className="nav flex-column">
@@ -34,6 +34,11 @@ export default function Layout({ children }) {
           {children}
         </Col>
       </Row>
+      <footer className={"text-center text-light p-3 " + style.footer}>
+        <Container>
+          <span className="align-middle">Version { APP_VERSION } - Created by DevTeam &copy; 2018-2019.</span>
+        </Container>
+      </footer>
     </div>
   );
 }
