1a43f5720c4bbf639fcb57e1f2db5dbd3e62862c
Vass Bence <vbencze2@gmail.com>
2020-01-19 15:58:57 +0100

    dark mode

--- components/button/index.js
+++ components/button/index.js
@@ -3,8 +3,11 @@ import cn from "classnames";
 
 // TODO itt egy pelda, hogy "big", "disabled", "small" es tarsai propokkal hogyan kell a css-t valtoztatni
 
-export default ({ children, peldaProp }) => (
-  <button className={cn(styles.default, peldaProp && styles.peldaProp)}>
+export default ({ children, peldaProp, ...rest }) => (
+  <button
+    className={cn(styles.default, peldaProp && styles.peldaProp)}
+    {...rest}
+  >
     {children}
   </button>
 );
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import Navbar from "../components/navbar";
 import { useAuth } from "../components/providers";
 import Button from "../components/button";
+import toggleDarkMode from "../lib/toggle-dark-mode";
 
 export default () => {
   const { user, loading } = useAuth();
@@ -14,54 +15,8 @@ export default () => {
         Logged in as {user?.full_name} with ID: {user?.id}.
         <Button>Ez egy gomb</Button>
         <Button peldaProp>Ez egy piros gomb</Button>
+        <Button onClick={toggleDarkMode}>Dark mode</Button>
       </div>
-
-      <style jsx>{`
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
-        }
-      `}</style>
     </Layout>
   );
 };
