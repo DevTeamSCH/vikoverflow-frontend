a7aeac4bdc65c1239eb9cac05ba1fc6dbc53d47f
Daniel Gal <daniel.gal@sch.bme.hu>
2020-02-11 23:04:05 +0100

    Style

--- src/pages/first-login.js
+++ src/pages/first-login.js
@@ -1,25 +1,55 @@
-import { useRouter } from "next/router";
+import { useState } from "react";
+import Router from "next/router";
 
-import Page from "../components/page";
 import Button from "../components/button";
+import TextArea from "../components/textarea";
+import TextField from "../components/textfield";
 
 export default () => {
-  const router = useRouter();
-  function onClick (e) {
+  const [text, setText] = useState("");
+
+  function handleSubmit (e) {
+    e.preventDefault();
     (async function setDisplayName () {
       await fetch("/api/v1/auth/display-name/", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
         method: "POST",
-        body: JSON.stringify({ displayName: "Den" })
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ displayName: text })
       });
+      Router.push("/");
     })();
   }
   return (
-    <Page pageTitle="First login">
-      <Button onClick={onClick}>Go</Button>
-    </Page>
+    <main className="container">
+      <h1>Kérlek, válassz magadnak felhasználónevet</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={text}
+          autoFocus
+          onChange={e => setText(e.target.value)}
+        />
+        <Button type="submit">Tovább</Button>
+      </form>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+
+        h1 {
+          font-size: 16px;
+          font-weight: 400;
+          text-align: center;
+        }
+
+        form > :global(input) {
+          margin-right: var(--gap-half);
+        }
+      `}</style>
+    </main>
   );
 };
